c13e665bdfb7d9fcc1a5c579edd1ae54c91d3671
Shashwat Singh <123892669+Shashron7@users.noreply.github.com>
2024-04-15 22:03:40 +0530

    some changes after dep

--- src/Components/ChatRoom.js
+++ src/Components/ChatRoom.js
@@ -1,121 +1,127 @@
-  import React, { useEffect, useRef} from "react";
-  import "./ChatRoom.css";
-  import { useState } from "react";
-  import { motion } from "framer-motion";
-  import {app, getAuth} from './Firebase';
-  import {getFirestore, addDoc, collection, serverTimestamp, query, orderBy, limit, onSnapshot} from 'firebase/firestore';
-  import Message from "./Message";
-  const db = getFirestore(app);  // Initialize Cloud Firestore and get a reference to the service
-  const auth=getAuth(app);
-  export default function ChatRoom() {
-    
-    const [text, setText]=useState(true);  //state to disable the button if there is nothing written
-    const [message, setMessage]=useState(""); //this state is to set the message
-    const [messages, setMessages] = useState([]);  //array of messages
-    const inputRef = useRef(null);  //reference to input field
-    const chatroomRef = useRef(null);  //for the chatroom for scrolling purposes
-    const buttonRef=useRef(null);
+import React, { useEffect, useRef } from "react";
+import "./ChatRoom.css";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { app, getAuth } from "./Firebase";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  serverTimestamp,
+  query,
+  orderBy,
+  limit,
+  onSnapshot,
+} from "firebase/firestore";
+import Message from "./Message";
+const db = getFirestore(app); // Initialize Cloud Firestore and get a reference to the service
+const auth = getAuth(app);
+export default function ChatRoom() {
+  const [text, setText] = useState(true); //state to disable the button if there is nothing written
+  const [message, setMessage] = useState(""); //this state is to set the message
+  const [messages, setMessages] = useState([]); //array of messages
+  const inputRef = useRef(null); //reference to input field
+  const chatroomRef = useRef(null); //for the chatroom for scrolling purposes
+  const buttonRef = useRef(null);
 
-    useEffect(()=>{
-      
-      const messagesRef=collection(db,'messages'); //taking a reference to the messages database
+  useEffect(() => {
+    const messagesRef = collection(db, "messages"); //taking a reference to the messages database
 
-      const q = query(messagesRef, orderBy('createdAt'), limit(100)); //querying the first 50 messages
+    const q = query(messagesRef, orderBy("createdAt"), limit(100)); //querying the first 50 messages
 
-      const unsubscribe = onSnapshot(q, (querySnapshot) => { //querysnapshot, means the current documents get are stored
-        const updatedMessages = [];  //we have an empty array that we will populate
-        querySnapshot.forEach((doc) => {
-          updatedMessages.push(doc.data());
-        });
-        setMessages(updatedMessages); //update the state
-        chatroomRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      //querysnapshot, means the current documents get are stored
+      const updatedMessages = []; //we have an empty array that we will populate
+      querySnapshot.forEach((doc) => {
+        updatedMessages.push(doc.data());
       });
-    return () => unsubscribe();   //we run this function everytime there is a render
-  }, []); 
+      setMessages(updatedMessages); //update the state
+      chatroomRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+    return () => unsubscribe(); //we run this function everytime there is a render
+  }, []);
 
+  const sendFunc = async (event) => {
+    event.preventDefault(); //preventing any default action of the submit button
+    const { uid, displayName, photoURL } = auth.currentUser; //getting the current user details and we know someone is definitely signed in
 
-    const sendFunc = async (event) => {
-      event.preventDefault(); //preventing any default action of the submit button
-      const { uid, displayName, photoURL } = auth.currentUser; //getting the current user details and we know someone is definitely signed in
-      
-      try {
-        console.log('Adding a new document...', displayName);
-        await addDoc(collection(db, "messages"), {             //this function adds a document in the database with the following key pair values
-          text: message,
-          name: displayName,
-          avatar: photoURL,
-          createdAt: serverTimestamp(),
-          uid,
-        });
-        console.log('Document successfully added!');  //after the promise resolves successfully
-        setMessage(''); // Reset message state after successful addition
-        inputRef.current.value='';
-        setText(true); //disable the button
-      } catch (error) {
-        console.error('Error adding document: ', error);
-      }
+    try {
+      console.log("Adding a new document...", displayName);
+      await addDoc(collection(db, "messages"), {
+        //this function adds a document in the database with the following key pair values
+        text: message,
+        name: displayName,
+        avatar: photoURL,
+        createdAt: serverTimestamp(),
+        uid,
+      });
+      console.log("Document successfully added!"); //after the promise resolves successfully
+      setMessage(""); // Reset message state after successful addition
+      inputRef.current.value = "";
+      setText(true); //disable the button
+    } catch (error) {
+      console.error("Error adding document: ", error);
     }
-    
-    return (
-      <div className="chatroom-wrapper">
-        <div className="chatroom-container" ref={chatroomRef}>
-          <motion.h3
-            initial={{ y: 300, fontSize: "100px", opacity: 1 }} // Initial state with opacity set to 1 and large font size
-            animate={{ y: 10, fontSize: "40px", opacity: 0 }} // Animation state with opacity set to 1 and smaller font size
-            transition={{ ease: "easeOut", duration: 2, delay: 3 }} // Delay of 2 seconds and transition duration of 4 seconds
-          >
-            This is the ChatRoom
-          </motion.h3> 
-          {messages.map((message, index) => (
-        <Message
-          key={index}
-          curr_uid={message.uid}
-          name={message.name}
-          url_pic={message.avatar}
-          text={message.text}
-        />
-      ))}
+  };
 
-        </div>
-        <div className="footer-chat">
-          <div className="mb-3 container">
-            <div class="d-flex justify-content-center">
-              <input
-                type="text"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                onChange={(e)=>{
-                  if (e.target.value.length > 0) {
-                    setText(false); //text boolean decides if we have disabled the button or not
-                    setMessage(e.target.value); //setting the message state here
-                  } else {
-                    setText(true);
-                  }
-                }}
-                onKeyDown={(e)=>{
-                  if(e.key==='Enter' && e.target.value.length > 0)
-                  {
-                    sendFunc(e);
-                    inputRef.current.value=''; //reset the input text to nothing
-                    setText(true); //disabling the button
-                  }
-                }}
-                ref={inputRef}
-              />
-              <button   
-                type="button"
-                className="btn btn-light"
-                style={{ marginLeft: "20px" }}
-                disabled={text}
-                onClick={(event)=>sendFunc(event)}
-                ref={buttonRef}
-              >
-                <i className="fa-regular fa-paper-plane"></i>
-              </button>
-            </div>
+  return (
+    <div className="chatroom-wrapper">
+      <div className="chatroom-container" ref={chatroomRef}>
+        <motion.h3
+          initial={{ y: 300, fontSize: "100px", opacity: 1 }} // Initial state with opacity set to 1 and large font size
+          animate={{ y: 10, fontSize: "40px", opacity: 0 }} // Animation state with opacity set to 1 and smaller font size
+          transition={{ ease: "easeOut", duration: 2, delay: 3 }} // Delay of 2 seconds and transition duration of 4 seconds
+        >
+          This is the ChatRoom
+        </motion.h3>
+        {messages.map((message, index) => (
+          <Message
+            key={index}
+            curr_uid={message.uid}
+            name={message.name}
+            url_pic={message.avatar}
+            text={message.text}
+          />
+        ))}
+      </div>
+      <div className="footer-chat">
+        <div className="mb-3 container">
+          <div class="d-flex justify-content-center">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              onChange={(e) => {
+                if (e.target.value.length > 0) {
+                  setText(false); //text boolean decides if we have disabled the button or not
+                  setMessage(e.target.value); //setting the message state here
+                } else {
+                  setText(true);
+                }
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && e.target.value.length > 0) {
+                  sendFunc(e);
+                  inputRef.current.value = ""; //reset the input text to nothing
+                  setText(true); //disabling the button
+                }
+              }}
+              ref={inputRef}
+            />
+            <button
+              type="button"
+              className="btn btn-light"
+              style={{ marginLeft: "20px" }}
+              disabled={text}
+              onClick={(event) => sendFunc(event)}
+              ref={buttonRef}
+            >
+              <i className="fa-regular fa-paper-plane"></i>
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,49 +1,53 @@
 import React, { useEffect, useState } from "react";
 import UseTypingEffect from "./UseTypingEffect";
-import './Welcome.css';
+import "./Welcome.css";
 
 import { useNavigate } from "react-router-dom";
-import {app, signInWithRedirect, getRedirectResult, GoogleAuthProvider, getAuth, getAdditionalUserInfo } from './Firebase'   //importing from Firebase.js
+import {
+  app,
+  signInWithRedirect,
+  getRedirectResult,
+  GoogleAuthProvider,
+  getAuth,
+  getAdditionalUserInfo,
+} from "./Firebase"; //importing from Firebase.js
+import { motion } from "framer-motion";
 
+import load from "./3-dots-bounce.svg";
+import pic from "./anim.gif";
+const googleAuthProvider = new GoogleAuthProvider();
 
-import load from './3-dots-bounce.svg';
-import pic from './anim.gif'
-const googleAuthProvider= new GoogleAuthProvider();
-
-
-const texts = [                         //typewriter text animation
+const texts = [
+  //typewriter text animation
   "Hello there!",
   "Welcome to ChatApp!",
   "This is a chat application ",
   "Enjoy chatting!",
 ];
 
-const auth=getAuth(app);  //auth is the authorization object
-
-
+const auth = getAuth(app); //auth is the authorization object
 
 export default function Welcome() {
   const [textIndex, setTextIndex] = useState(0);
-  const [loading, setLoading]=useState(false);
-  const textToShow = UseTypingEffect(texts[textIndex], 50, true); 
-  const navigate= useNavigate();
+  const [loading, setLoading] = useState(false);
+  const textToShow = UseTypingEffect(texts[textIndex], 50, true);
+  const navigate = useNavigate();
 
-  const handleSignInWithGoogle = async() => {
+  const handleSignInWithGoogle = async () => {
     try {
-      
-      await signInWithRedirect(auth, googleAuthProvider);   //Doesnt return anything, the results and errors are handled by getRedirectResult
+      await signInWithRedirect(auth, googleAuthProvider); //Doesnt return anything, the results and errors are handled by getRedirectResult
     } catch (error) {
       console.log(error);
     }
   };
 
-
-  useEffect(() => {       //this is for the typewriter animation.
+  useEffect(() => {
+    //this is for the typewriter animation.
     const intervalId = setInterval(() => {
       setTextIndex((prevIndex) =>
         prevIndex >= texts.length - 1 ? 0 : prevIndex + 1
       );
-    }, 3000); 
+    }, 3000);
 
     return () => {
       clearInterval(intervalId);
@@ -51,8 +55,8 @@ export default function Welcome() {
   }, []);
 
   //seeing if there is already a user signed in or not
-  
-  const debugRedirectResult = async () => {  
+
+  const debugRedirectResult = async () => {
     try {
       setLoading(true);
       const result = await getRedirectResult(auth);
@@ -60,59 +64,81 @@ export default function Welcome() {
         const details = getAdditionalUserInfo(result);
         console.log(details);
         console.log("User logged in:", result.user.displayName);
-        let name_user=result.user.displayName;
-        navigate('/in');  //navigating to the signedinpage and passing the name
+        let name_user = result.user.displayName;
+        navigate("/in"); //navigating to the signedinpage and passing the name
       } else {
         console.log("No user logged in");
       }
     } catch (error) {
       console.log("Error retrieving redirect result:", error);
-    } finally{
+    } finally {
       setLoading(false);
     }
   };
-  useEffect(() => {       //after rendering we are checking if any user is logged in or not
-  debugRedirectResult();
+  useEffect(() => {
+    //after rendering we are checking if any user is logged in or not
+    debugRedirectResult();
   }, []);
-  
-
 
   return (
-    <div className="whole" >
-      <div className="container">
-        {loading ? <img src={load} style={{height: '100px'}}/> : (<div
-          className="login"
-          style={{fontFamily: "Reddit Mono" }}
+    <div className="whole">
+      <div className="container-welcome">
+      
+        {loading ? (
+          <img src={load} style={{ height: "100px" }} />
+        ) : (
+        <>
+          <div className="texts">
+        <motion.h1
+          style={{ fontFamily: "Reddit Mono" }}
+          initial={{ y: 300, fontSize: "50px", opacity: 0 }}
+          animate={{ y: 50, fontSize: "50px", opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 1 }}
         >
-          <div  >
-            <h3 style={{ marginBottom: "60px" }}>Get started chatting....</h3>
-            <div className="d-flex flex-column mb-3 button_pic">
-            <img
-              src={pic}
-              style={{ height: "100 px", width: "200 px" , marginTop: '50px'}}
-              className="p-2 img-chat"
-            />
-              <button
-                type="button"
-                className="btn btn-primary btn-lg custom-but p-2"
-                style={{width: "500px"}}
-                onClick={handleSignInWithGoogle}
+          {textToShow}
+        </motion.h1>
+      </div>
+          <div className="login" style={{ fontFamily: "Reddit Mono" }}>
+            <div>
+              <motion.h3
+                style={{ marginBottom: "60px" }}
+                initial={{ y: 300, opacity: 0 }}
+                animate={{ y: 50, opacity: 1 }}
+                transition={{ ease: "easeOut", duration: 1, delay: 2 }}
               >
-                Login with Google
-                <i class="fa-brands fa-google" style={{marginLeft: "20px"}}></i>
-              </button>
-            
-
-           
+                Get started chatting....
+              </motion.h3>
+              <div className="d-flex flex-column mb-3 button_pic">
+                <img
+                  src={pic}
+                  style={{
+                    height: "100 px",
+                    width: "200 px",
+                    marginTop: "50px",
+                  }}
+                  className="p-2 img-chat"
+                />
+                <motion.button
+                  type="button"
+                  className="btn btn-primary btn-lg custom-but p-2"
+                  style={{ width: "500px" }}
+                  onClick={handleSignInWithGoogle}
+                  initial={{ y: 300, fontSize: "10px", opacity: 0 }}
+                  animate={{ y: 50, fontSize: "30px", opacity: 1 }}
+                  transition={{ ease: "easeOut", duration: 1, delay: 2 }}
+                >
+                  Login with Google
+                  <i
+                    class="fa-brands fa-google"
+                    style={{ marginLeft: "20px" }}
+                  ></i>
+                </motion.button>
+              </div>
             </div>
           </div>
-          <h1 style={{ fontFamily: "Reddit Mono", marginTop: "100px" }}>
-        {textToShow}
-      </h1>
-        </div>
+          </>
         )}
       </div>
-      
     </div>
   );
 }
