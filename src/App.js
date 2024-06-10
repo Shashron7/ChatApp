f2317c43333ba26f826ee314ff2dba7ac86aea07
Shashwat Singh <123892669+Shashron7@users.noreply.github.com>
2024-06-10 18:02:09 +0530

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Signedin from "./Components/Signedin";
 import ChatRoom from "./Components/ChatRoom";
 import { UserProvider } from "./Components/UserContext";
-
+import { app, getAuth } from "./Components/Firebase";
+import { useEffect } from "react";
 
 function App() {
+  
   return (
     <div className="App">
       <UserProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Welcome />}></Route>
-          <Route path="/in" element={<Signedin />}></Route>
-          <Route path="/chat" element={<ChatRoom />}></Route>
-        </Routes>
-      </Router>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Welcome />}></Route>
+            <Route path="/in" element={<Signedin />}></Route>
+            <Route path="/chat" element={<ChatRoom />}></Route>
+          </Routes>
+        </Router>
       </UserProvider>
     </div>
   );
